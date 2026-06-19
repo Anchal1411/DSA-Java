31a7c8b2a7c69fab95905327fe0d3f88315a8a7c
Anchal Thakur <anchal.thakur0418@gmail.com>
2026-06-20 00:04:25 +0530

    Added Majority Element

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,26 @@
+public class MajorityElement {
+  
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println("Majority element in nums1: " + majorityElement(nums1)); // Output: 3
+        System.out.println("Majority element in nums2: " + majorityElement(nums2)); // Output: 2
+    }
+}
+    
+}
