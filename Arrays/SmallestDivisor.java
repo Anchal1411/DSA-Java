67af11dea08ca20142432bc404a936cfa8e16ee3
Anchal Thakur <anchal.thakur0418@gmail.com>
2026-06-27 23:55:14 +0530

    Added Find Smallest Divisor

---
+++ Arrays/SmallestDivisor.java
@@ -0,0 +1,35 @@
+public class SmallestDivisor {
+
+    
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low = 1;
+        int high = 0;
+
+        for (int x : nums) {
+            high = Math.max(high, x);
+        }
+
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int sum = 0;
+
+            for (int num : nums) {
+                sum += (num + mid - 1) / mid;
+            }
+
+            if (sum <= threshold) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
+    
+
