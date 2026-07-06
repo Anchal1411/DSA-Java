389e89d5becbc138a0eb86bbeebccb337e4060a4
Anchal Thakur <anchal.thakur0418@gmail.com>
2026-07-06 16:26:25 +0530

    Added Search In 2D Matrix II

---
+++ Arrays/SearchIn2DMatrixII.java
@@ -0,0 +1,24 @@
+public class SearchIn2DMatrixII {
+
+    public boolean searchMatrix(int[][] arr, int target) {
+
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int i = 0;
+        int j = m-1;
+
+        while(i < n && j >=0){
+          if(arr[i][j] == target) return true;
+
+          if(target > arr[i][j]){
+            i++;
+          } else {
+            j--;
+          }
+
+        }
+        return false;
+    }
+    
+}
