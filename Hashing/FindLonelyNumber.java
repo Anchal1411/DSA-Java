d9dfa21886294126074e0c1ef9cae8899d47345f
Anchal Thakur <anchal.thakur0418@gmail.com>
2026-08-15 16:30:45 +0530

    Added Find Lonely Number in an Array

---
+++ Hashing/FindLonelyNumber.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class FindLonelyNumber {
+    public List<Integer> findLonely(int[] nums) {
+        List <Integer> list = new ArrayList<>();
+
+        HashMap <Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+             
+        }
+
+        for(int num : nums){
+            if(map.get(num) == 1 && 
+            !map.containsKey(num - 1) &&
+            !map.containsKey(num + 1)){
+
+            list.add(num);
+            }
+
+            
+        }
+           return list;
+        
+    }
+
+ 
+}
