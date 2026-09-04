a4a936826b0b47464ae443daa323d27e86f55ab3
Anchal Thakur <anchal.thakur0418@gmail.com>
2026-09-04 16:24:21 +0530

    Added Reverse a String using Stack

---
+++ Stack/ReverseString.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ReverseString {
+
+    public static String reverse(String str){
+
+        Stack<Character> s = new Stack<>();
+        
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx ++;
+        }
+
+        StringBuilder result = new StringBuilder("");
+        result.append(s.pop());
+
+        return result.toString();
+         
+    }
+
+    public static void main(String args[]){
+        String str = "abc";
+        System.out.println(reverse(str));
+
+    }
+    
+}
